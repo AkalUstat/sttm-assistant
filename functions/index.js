54e2af7821159963aeed71cc44b1a0daf2c12263
akal-ustatsingh <akalustat.singh@gmail.com>
2019-05-30 15:57:20 -0700

    starting tutorial 2

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
+const {
+  dialogflow,
+  Permission,
+  Suggestions,
+} = require('actions-on-google');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
