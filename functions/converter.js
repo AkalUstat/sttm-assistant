074fbced9749ae739ef7269460363c5af66d988b
akal-ustatsingh <akalustat.singh@gmail.com>
2019-06-02 10:52:24 -0700

    complete fetchHukam and translit functions

--- functions/converter.js
+++ functions/converter.js
@@ -1,1 +1,11 @@
 const anvaad = require('anvaad');
+const hukam = require('./fetch_hukam')
+
+
+let convertedVerse = [];
+async function converter() {
+  var result = await hukam.fetchShabad();
+  for (let i =0; i < result.length; i++){
+    convertedVerse.push(anvaad.translit(result[i]));
+  }
+}
--- functions/fetch_hukam.js
+++ functions/fetch_hukam.js
@@ -1,18 +1,31 @@
 const getJSON = require('get-json');
 
-const hasSatgurPrasaad = true; 
-function fetchMangal(){
+// const hasSatgurPrasaad = true; 
+// function fetchMangal(){
+//   return new Promise(resolve => {
+//     getJSON('https://api.banidb.com/v2/hukamnamas/today', (error, response) => {
+//       if (!error) {
+//         const sID = respabads[0].shabadInfo.shabadId;
+//         const raagAndWriter = response.shabads[0].verses[0].larivaar.gurmukhi; 
+//         let remainingMangal = '';
+//         if(response.shabads[0].verses[1].verse.gurmukhi === '<> siqgur pRswid ]'){
+//           remainingMangal = '<> siqgur pRswid ]';
+//         }
+//         resolve(raagAndWriter + remainingMangal);
+//       }
+//    }
+//   )}
+// )}
+let retrievedShabad = [];
+function fetchShabad(){
   return new Promise(resolve => {
     getJSON('https://api.banidb.com/v2/hukamnamas/today', (error, response) => {
-      if (!error) {
-        const sID = response.shabads[0].shabadInfo.shabadId;
-        const raagAndWriter = response.shabads[0].verses[0].larivaar.gurmukhi; 
-        let remainingMangal = ' ';
-        if(response.shabads[0].verses[1].verse.gurmukhi === '<> siqgur pRswid ]'){
-          remainingMangal = '<> siqgur pRswid ]';
-        }
-        resolve(raagAndWriter + remainingMangal);
+      if(!error) {
+       for (let looper =0; looper < response.shabads[0].verses.length - 1; looper++) {
+         retrievedShabad[looper] = response.shabads[0].verses[looper].verse.gurmukhi;
+       }
+       resolve(retrievedShabad);
       }
-   }
-  )}
-)}
+    })
+  })
+}
